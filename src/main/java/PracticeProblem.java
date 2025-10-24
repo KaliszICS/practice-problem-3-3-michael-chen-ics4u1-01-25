96136911f9752db92f528caee076bf8f032828ff
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-10-24 16:13:16 -0400

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(perms("null"));
 	}
 
 	public static void q1() {
@@ -24,4 +26,41 @@ public class PracticeProblem {
 		//Write question 5 code here
 	}
 
+	public static ArrayList<String> perms(String a) {
+		ArrayList<String> permutations = new ArrayList<String>();
+
+		permsHelper(a, permutations, "");
+		return permutations;
+	}
+
+	public static void permsHelper(String unused, ArrayList<String> permutations, String used) {
+		if (unused.strip() == "") {
+			permutations.add(used);
+		}
+
+		for (int i = 0; i < unused.length(); i++) {
+			String usedLetters = used + unused.charAt(i);
+			String unusedLetters = unused.substring(0, i) + unused.substring(i + 1);
+			permsHelper(unusedLetters, permutations, usedLetters);
+		}
+	}
+
+	public static ArrayList<String> permsUnique(String a) {
+		ArrayList<String> uniquePermutations = new ArrayList<String>();
+
+		permsUniqueHelper(a, uniquePermutations, "");
+		return uniquePermutations;
+	}
+
+	public static void permsUniqueHelper(String unused, ArrayList<String> uniquePermutations, String used) {
+		if (unused.strip() == "" && !uniquePermutations.contains(used)) {
+			uniquePermutations.add(used);
+		}
+
+		for (int i = 0; i < unused.length(); i++) {
+			String usedLetters = used + unused.charAt(i);
+			String unusedLetters = unused.substring(0, i) + unused.substring(i + 1);
+			permsUniqueHelper(unusedLetters, uniquePermutations, usedLetters);
+		}
+	}
 }
